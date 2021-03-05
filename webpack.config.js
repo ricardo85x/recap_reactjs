2b6ecb1b80a568d2e7565ddafc4e7fede4bef06f
Ricardo <ricardoinliner@yahoo.com>
2021-03-05 11:48:06 -0300

    Webpack configuration
    Webpack configured and it is already generating the bundle.js and hosting the live server

---
+++ webpack.config.js
@@ -0,0 +1,26 @@
+const path = require('path')
+
+module.exports = {
+    entry: path.resolve(__dirname, 'src', 'index.js'),
+    output: { 
+        path: path.resolve(__dirname, 'public'),
+        filename: 'bundle.js'
+    },
+
+    devServer: {
+        contentBase: path.join(__dirname, 'public'),
+        port: 3367,
+    },
+
+    module: {
+        rules: [
+            {
+                test: /\.js$/,
+                exclude: /node_modules/,
+                use: {
+                    loader: 'babel-loader'
+                }
+            }
+        ]
+    }
+}
