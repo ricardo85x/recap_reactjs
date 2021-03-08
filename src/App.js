5ffcfac8187cb95d776880f6cea79995d3898844
ricardo <ricardo85x@gmail.com>
2021-03-08 10:59:07 -0300

    add basic property functionality

--- src/App.js
+++ src/App.js
@@ -1,7 +1,19 @@
 import React from 'react'
+import Header from './components/Header'
 
 function App() {
-    return <h1>Hello world</h1>
+    return (
+    
+    <>
+        <Header title="Homepage">
+			<ul>
+				<li>Home page</li>
+				<li>Projects</li>
+		    </ul>
+		</Header>
+        <h1>Hello world</h1>
+    </>
+    )
 }
 
 export default App;
