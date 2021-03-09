ceafc77d035352c9c6cec38ae3c5e0087b88668f
ricardo <ricardo85x@gmail.com>
2021-03-09 10:25:33 -0300

    import css inside js

--- webpack.config.js
+++ webpack.config.js
@@ -20,6 +20,19 @@ module.exports = {
                 use: {
                     loader: 'babel-loader'
                 }
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use: [
+                    {
+                        loader: 'style-loader'
+                    },
+                    {
+                        loader: 'css-loader'
+                    }
+                ]
+
             }
         ]
     }
